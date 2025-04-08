92831b36ab23071b10ddb7bcd5a520bb2e542db3
Yingzhao Ouyang <7325793+KunojiLym@users.noreply.github.com>
2025-04-08 15:07:17 +0000

    Deployed

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-($$5ubib8=2vmd3rp%(@7(edvj-%yle@d^wg%@=pnd^hbvg)ls
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[REPLACE-WITH-CODESPACE-URL]']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'psychic-robot-v479v7q6vhvg5-8000.app.github.dev']
 
 
 # Application definition
--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -6,7 +6,7 @@ from .models import User, Team, Activity, Leaderboard, Workout
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    base_url = 'http://[REPLACE-WITH-CODESPACE-URL]-8000.app.github.dev/'
+    base_url = 'https://psychic-robot-v479v7q6vhvg5-8000.app.github.dev/'
     return Response({
         'users': base_url + 'api/users/?format=api',
         'teams': base_url + 'api/teams/?format=api',
